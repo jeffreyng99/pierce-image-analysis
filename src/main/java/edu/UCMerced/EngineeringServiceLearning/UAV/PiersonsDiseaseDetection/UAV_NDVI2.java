ccf2ebf938c9d8683a80b48f2b37ebdc74cce628
Patricia Kaye Mendoza <kindapatriciaah@gmail.com>
2016-10-11 15:58:50 -0700

    NDVI Code Readjustments
    removed some code and renamed some variables to have a clearer
    understanding, along with comments

--- src/main/java/edu/UCMerced/EngineeringServiceLearning/UAV/PiersonsDiseaseDetection/UAV_NDVI2.java
+++ src/main/java/edu/UCMerced/EngineeringServiceLearning/UAV/PiersonsDiseaseDetection/UAV_NDVI2.java
@@ -9,39 +9,29 @@ import java.io.*;
 
 public class UAV_NDVI2 {
 
-	public static void main(String[] args) {
+	public static void NDVIProcessing(String inputImagePath,boolean isRedFilter) {
 
-		System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
-		InputStreamReader converter = new InputStreamReader(System.in);
-		BufferedReader in = new BufferedReader(converter);
+		//System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
 		System.out.println(System.getProperty("user.dir"));
-		Mat vineyard;
-		String imagepath;
+		Mat inputImageMatrix;
 
 		try{
 
-			System.out.println("Enter the exact path of the Red-Near-Infared stiched image: ");
-			imagepath = in.readLine();
-			vineyard = Highgui.imread(imagepath);
 
-			String filter = "";
-			while(filter.compareToIgnoreCase("red") != 0 && filter.compareToIgnoreCase("blue") != 0){
-				System.out.println("Red Filter or Blue Filter? (red|blue): ");
-				filter = in.readLine();
-			}
+			inputImageMatrix = Highgui.imread(inputImagePath);
 
 			Mat gray_vin = new Mat();
 			Mat yellow_vin = new Mat();
-			Imgproc.cvtColor(vineyard, gray_vin, Imgproc.COLOR_RGB2GRAY);
+			Imgproc.cvtColor(inputImageMatrix, gray_vin, Imgproc.COLOR_RGB2GRAY);
 			Imgproc.cvtColor(gray_vin, yellow_vin, Imgproc.COLOR_GRAY2RGB);
 
 			Mat ndvi = new Mat();
-			ndvi = vineyard;
+			ndvi = inputImageMatrix;
 			Mat ndvi2 = new Mat();
-			Imgproc.cvtColor(vineyard, ndvi2, Imgproc.COLOR_RGB2GRAY);
-			for(int i=0; i<ndvi.rows(); i++){
+			Imgproc.cvtColor(inputImageMatrix, ndvi2, Imgproc.COLOR_RGB2GRAY);
+			for(int i=0; i<ndvi.rows(); i++){ //loops through each pixel
 				for(int j=0; j<ndvi.cols(); j++){
-					double[] bands = vineyard.get(i, j);
+					double[] bands = inputImageMatrix.get(i, j); //returns the value of blue, green & red for each index respectively
 					double blue = bands[0];
 					double green = bands[1];
 					double red = bands[2];
@@ -51,7 +41,7 @@ public class UAV_NDVI2 {
 
 					////using blue channel as NIR, assuming red filter
 					////blue = NIR, green = green+NIR, red = red+NIR
-					if(filter.compareToIgnoreCase("red") == 0){
+					if(isRedFilter){
 						nir = blue;
 						red = red - nir;
 						calc_ndvi = (nir - red)/(nir + red);
@@ -60,7 +50,7 @@ public class UAV_NDVI2 {
 
 					////using red channel as NIR, assuming blue filter
 					////blue = blue + NIR, green = green+NIR, red = NIR
-					if(filter.compareToIgnoreCase("blue") == 0){
+					else{
 						nir = red;
 						blue = blue - red;
 						calc_ndvi = (nir - blue)/(nir + blue);
@@ -83,7 +73,7 @@ public class UAV_NDVI2 {
 					//double[] out_ndvi = {green, red, nir};
 					//double[] out_ndvi = {255*(1+calc_ndvi)/2, green, nir};
 					//double[] out_ndvi = {blue_ndvi, nir, red_ndvi};
-					ndvi.put(i, j, out_ndvi);
+					ndvi.put(i, j, out_ndvi); //coloring pixel to NDVI scale based on calculation
 					ndvi2.put(i,j, 255*(calc_ndvi+1)/2);
 
 					if(calc_ndvi < thresh){
