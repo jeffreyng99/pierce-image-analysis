6542b4be72fc35c9f818245cd92b3562908c959b
Chris Cruz <cdelacruz10@ucmerced.edu>
2018-09-19 15:16:05 -0700

    Deleted GUItest.form + Comments Added

--- src/main/java/edu/UCMerced/EngineeringServiceLearning/UAV/PiersonsDiseaseDetection/UserInterface.java
+++ src/main/java/edu/UCMerced/EngineeringServiceLearning/UAV/PiersonsDiseaseDetection/UserInterface.java
@@ -16,30 +16,35 @@ import java.util.Set;
  * Created by Buraaq Alrawi on 10/11/2016.
  */
 public class UserInterface {
-    private JButton OpenFileButton;
-    private JTextField InputFile1;
+
     private JPanel Jpanel;
-    private JTextField OutputFile1;
-    private JButton OutputFileButton;
-    private JButton Analyze;
-    private JTextPane textPane1;
+
+    private JButton btnInputFile;
+    private JButton btnOutputFile;
+    private JButton btnAnalyze;
+
+    private JTextField txtInputFile;
+    private JTextField txtOutputFile;
+
+
+    private JTextPane txpError;
 
     public UserInterface() {
-        OpenFileButton.addActionListener(new ActionListener() {
+        btnInputFile.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                InputFile1.setText(FilePicker(e));
+                txtInputFile.setText(FilePicker(e));
             }
         });
-        OutputFileButton.addActionListener(new ActionListener() {
+        btnOutputFile.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                OutputFile1.setText(FileOutput(e));
+                txtOutputFile.setText(FileOutput(e));
             }
         });
 
-        Analyze.addActionListener(new ActionListener() {
+        btnAnalyze.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                if (InputFile1.getText().isEmpty() || OutputFile1.getText().isEmpty()) {
-                    textPane1.setText("Pick folders, human.");
+                if (txtInputFile.getText().isEmpty() || txtOutputFile.getText().isEmpty()) {
+                    txpError.setText("Select the folder(s) location!");
                     return;
                 }
                     new Thread()
@@ -48,16 +53,16 @@ public class UserInterface {
                         {
                             try
                             {
-                                thisDoesPrettyMuchEverything(InputFile1.getText(), OutputFile1.getText());
+                                OnBtnAnalyze(txtInputFile.getText(), txtOutputFile.getText());
                             }
                             catch(IOException ex)
                             {
                                 JOptionPane.showMessageDialog(null,"Error,IOException");
-                                textPane1.setText(ex.getMessage());
+                                txpError.setText(ex.getMessage());
                             }
                             catch (InterruptedException ex)
                             {
-                                textPane1.setText(ex.getMessage());
+                                txpError.setText(ex.getMessage());
                                 JOptionPane.showMessageDialog(null,"Error,InterruptedException");
                             }
 
@@ -68,34 +73,34 @@ public class UserInterface {
             });}
 
 
-    private String FilePicker(ActionEvent e) {
+    private String FilePicker(ActionEvent e) { // Allows for the selecting of a folder where images are.
         JFileChooser fileChooser = new JFileChooser();
         fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
         System.setProperty("java.library.path", "/opencv/build/java/x64");
-        int returnValue = fileChooser.showDialog(null, "Choose Input Folder");
+        int returnValue = fileChooser.showDialog(null, "Select folder with images!");
         if (returnValue == JFileChooser.APPROVE_OPTION) {
             return fileChooser.getSelectedFile().getPath();
         } else {
-            return "Pick a folder.";
+            return "Select folder with images!";
         }
     }
     public void SetText(String instruct){
-        textPane1.setText(instruct);
+        txpError.setText(instruct);
     }
 
-    private String FileOutput(ActionEvent e){
+    private String FileOutput(ActionEvent e){ // Allows for the selecting of folder to save NDVI stitch.
         JFileChooser fileChooser1 = new JFileChooser();
         fileChooser1.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-        int returnValue = fileChooser1.showDialog(null, "Choose Output Folder");
+        int returnValue = fileChooser1.showDialog(null, "Select folder to save NDVI image!");
         if (returnValue == JFileChooser.APPROVE_OPTION) {
             return fileChooser1.getSelectedFile().getPath();
         } else{
-            return "Pick output folder.";
+            return "Select folder to save NDVI image!";
         }
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { //Creating the user GUI and exporting the
         JFrame frame = new JFrame("UserInterface");
         frame.setContentPane(new UserInterface().Jpanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -103,7 +108,7 @@ public class UserInterface {
         frame.setVisible(true);
         try
         {
-            String fullPath = ExportResource("/opencv_java2411.dll");
+            String fullPath = ExportResource("/opencv_java2411.dll"); // Outputs necessary .dll to use opencv library.
             System.load(fullPath);
         }
         catch (Exception e)
@@ -124,7 +129,7 @@ public class UserInterface {
         OutputStream resStreamOut = null;
         String jarFolder;
         try {
-            stream = UserInterface.class.getResourceAsStream(resourceName);//note that each / is a directory down in the "jar tree" been the jar the root of the tree
+            stream = UserInterface.class.getResourceAsStream(resourceName);
             if(stream == null) {
                 throw new Exception("Cannot get resource \"" + resourceName + "\" from Jar file.");
             }
@@ -146,21 +151,18 @@ public class UserInterface {
         return jarFolder + resourceName;
     }
 
-    public void thisDoesPrettyMuchEverything(String inputFolder, String outputFolder) throws IOException, InterruptedException {
-        String Instruct = "Hello";
-        String stitchedImage="";
-        //In other words, useless stuff. take code from uaveWindowed function that isn't GUI stuff.
-        SetText("Microsoft ICE will now open, \n" +
-                "go through each step and click on 'Next', which can be found in the top right corner. \n" +
-                "When in step 4 click on 'Export to disk', and then click save." ); //letting user know that the program used to stitch the images will open on its own
-        //Telling user to move through the the steps by clicking next in microsoft ICE
-        //Telling user to export the stitched image into the default folder
+    public void OnBtnAnalyze(String inputFolder, String outputFolder) throws IOException, InterruptedException {
+
+        String stitchedImage;
+        SetText("Microsoft ICE now launching...Please select the last step at the top right corner!");
         File file = new File("blah.spj");  //Creates a .spj file in memory
         BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+
         //Writing inside the .spj file
         writer.write("<?xml version=\"1.0\" encoding =\"utf-8\"?>"); writer.newLine();
         writer.write("<stitchProject version=\"2.0\" cameraMotion=\"automatic\">"); writer.newLine();
         writer.write("<sourceImages>"); writer.newLine();
+
         //Writing the location of the images in the .spj file
         File inputImages = new File(inputFolder);
         File[] listOfFiles = inputImages.listFiles();
@@ -176,16 +178,16 @@ public class UserInterface {
         writer.close();
 
         Runtime rt = Runtime.getRuntime();
-        rt.exec("cmd.exe /c blah.spj"); //Opens ICE and then stitches the images listed in the .spj file. We would change this line if we were to stitch with another program
-        //This should open up ICE and wait for the user to start the actual stitching process. The user can press next for each step in ICE. ICE will save the stitched image
-        //under the name of the first image with a _stitch added to the end. It will also save this file in the documents folder by default.
+        rt.exec("cmd.exe /c blah.spj"); //Opens ICE and then stitches the images listed in the .spj file
+
         String imageName = listOfFiles[0].getName();
         imageName = FilenameUtils.removeExtension(imageName);
-        String userprofile = System.getenv("userprofile");
-        stitchedImage = userprofile + "\\Documents"+"\\"+imageName+"_stitch.jpg";
+
+        String profileUser = System.getenv("userprofile");
+        stitchedImage = profileUser + "\\Documents"+"\\"+imageName+"_stitch.jpg";
+
         File stitchedImageFile = new File(stitchedImage);
-        File outputImages = new File(outputFolder);
-        File[] listofFilesDocuments = stitchedImageFile.listFiles();
+
         if (stitchedImageFile.exists())
             stitchedImageFile.delete();
         File outputFile = new File(outputFolder+"\\"+imageName+"_stitch.jpg");
@@ -220,7 +222,7 @@ public class UserInterface {
 	        Highgui.imwrite(stitchedImage, matObject2);
 	        SetText("Done. You can find the image in " + outputFolder);
 
-	        System.exit(0);
+	        //System.exit(0); //Do not close APP
 
     }
 }
