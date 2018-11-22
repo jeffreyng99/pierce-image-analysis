156dbea7bdec41c912d9b463a9dd493c92b37a34
Chris Cruz <cdelacruz10@ucmerced.edu>
2018-11-22 02:23:19 -0800

    Compile Test 2

---
+++ src/main/java/edu/UCMerced/EngineeringServiceLearning/UAV/PiersonsDiseaseDetection/GUI/UI_4_Controller.java
@@ -0,0 +1,15 @@
+package edu.UCMerced.EngineeringServiceLearning.UAV.PiersonsDiseaseDetection.GUI;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UI_4_Controller implements Initializable {
+
+    public void initialize(URL url, ResourceBundle rb) {
+        // loadUI("UI_3.fxml");
+    }
+
+
+}
